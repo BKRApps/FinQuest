e33cd0f54503b73badb3113a96301ca464aef09a
Kumar Reddy <kumarreddy72@gmail.com>
2025-07-05 14:47:17 +0530

    project setup

---
+++ FinQuest/FinQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinQuestApp.swift
+//  FinQuest
+//
+//  Created by Kumar on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FinQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinQuestTests/FinQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinQuestTests.swift
+//  FinQuestTests
+//
+//  Created by Kumar on 05/07/25.
+//
+
+import Testing
+@testable import FinQuest
+
+struct FinQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
